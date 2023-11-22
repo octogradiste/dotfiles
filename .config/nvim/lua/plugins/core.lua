eaced27c2063a75c9cc0d6f6fe73ac2c559af84f
octogradiste <colin.berger@outlook.com>
2023-11-22 22:36:58 +0100

    Added neovim config

---
+++ .config/nvim/lua/plugins/core.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = false,
+    },
+  },
+}
